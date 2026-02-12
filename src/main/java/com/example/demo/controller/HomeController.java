202ebd304746fae88d21c65e66c265feec8406f8
Cristian marrugo <cristian.maba2005@outlook.com>
2026-02-12 14:50:26 -0500

    feat: modifiqué el archivo HomeController

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,7 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "redirect:/pin";
+    public String index(HttpSession session) {
+
+        if (session.getAttribute("empleado") == null) {
+            return "redirect:/pin";
+        }
+
+        return "index";
     }
 }
+
