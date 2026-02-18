05f69f71f91b6a461b76fb7d7276f8c56ff606ac
Cristian marrugo <cristian.maba2005@outlook.com>
2026-02-18 13:05:51 -0500

    add: agregué la clase HomeController anteriormente borrada

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(HttpSession session) {
+
+        if (session.getAttribute("empleado") == null) {
+            return "redirect:/pin";
+        }
+
+        return "index";
+    }
+}
+
