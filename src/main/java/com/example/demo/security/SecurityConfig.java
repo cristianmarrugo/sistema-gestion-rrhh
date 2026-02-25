e2561784a83275f15b3dd9a44d9c902dd706609a
Cristian marrugo <cristian.maba2005@outlook.com>
2026-02-25 15:05:56 -0500

    add: nueva clase de security

---
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // 🔥 Permitimos todo
+                )
+                .formLogin(form -> form.disable())
+                .logout(logout -> logout.disable());
+
+        return http.build();
+    }
+}
