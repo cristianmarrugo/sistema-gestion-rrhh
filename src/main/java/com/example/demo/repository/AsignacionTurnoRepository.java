81a4a62b9f1045b3b1d99a06c6804981ac53bcd8
Cristian marrugo <cristian.maba2005@outlook.com>
2026-03-09 22:34:54 -0500

    add: aregué la clase repository de asignacion

---
+++ src/main/java/com/example/demo/repository/AsignacionTurnoRepository.java
@@ -0,0 +1,24 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.AsignacionTurno;
+import com.example.demo.model.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AsignacionTurnoRepository extends JpaRepository<AsignacionTurno, Long> {
+
+    Optional<AsignacionTurno> findByEmpleadoAndFecha(Empleado empleado, LocalDate fecha);
+
+    List<AsignacionTurno> findByEmpleadoAndFechaBetween(
+            Empleado empleado,
+            LocalDate inicio,
+            LocalDate fin
+    );
+
+    List<AsignacionTurno> findByFecha(LocalDate fecha);
+}
