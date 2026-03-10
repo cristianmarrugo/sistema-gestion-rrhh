76c248c20cb9a02d6c4c88fc18402be9820a9823
Cristian marrugo <cristian.maba2005@outlook.com>
2026-03-09 22:35:32 -0500

    feat: agregué el metodo buscar empledo entre fech

--- src/main/java/com/example/demo/repository/AsistenciaRepository.java
+++ src/main/java/com/example/demo/repository/AsistenciaRepository.java
@@ -5,8 +5,11 @@ import com.example.demo.model.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface AsistenciaRepository extends JpaRepository<Asistencia, Long> {
     Optional<Asistencia> findByEmpleadoAndFecha(Empleado empleado, LocalDate fecha);
+
+    List<Asistencia> findByEmpleadoAndFechaBetween(Empleado empleado, LocalDate fechaInicio, LocalDate fechaFin);
 }
