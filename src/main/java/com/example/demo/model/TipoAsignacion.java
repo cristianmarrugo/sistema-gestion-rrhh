6bae8742d05b494818f56cdf89c23b88b16d3279
Cristian marrugo <cristian.maba2005@outlook.com>
2026-03-09 22:34:01 -0500

    add: enum de tipo asignacion

---
+++ src/main/java/com/example/demo/model/TipoAsignacion.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+
+public enum TipoAsignacion {
+    NORMAL,       // Asignación normal del turno
+        LIBRE,        // Día libre
+        VACACIONES,   // En vacaciones
+        PERMISO,      // Permiso aprobado
+        CAMBIO        // Cambio de turno con otro empleado
+    }
+
