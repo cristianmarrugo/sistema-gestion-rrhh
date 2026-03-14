864b3b77c23d37bf884405666f514529c74e1c11
Cristian marrugo <cristian.maba2005@outlook.com>
2026-03-13 21:10:45 -0500

    add: config para auditoria

---
+++ src/main/java/com/example/demo/security/AuditConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class AuditConfig {
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        // Esto le dice a Spring de dónde sacar el usuario logueado
+        return () -> {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if (auth == null || !auth.isAuthenticated()) return Optional.of("SYSTEM");
+            return Optional.of(auth.getName());
+        };
+    }
+}
