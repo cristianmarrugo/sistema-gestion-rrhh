efed40bcb563b3961f636dd17bf6294b324a7480
Cristian marrugo <cristian.maba2005@outlook.com>
2026-05-06 16:21:24 -0500

    feat: establecer fecha de creacion de la notificacion

--- src/main/java/com/example/demo/service/NotificacionService.java
+++ src/main/java/com/example/demo/service/NotificacionService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -23,6 +24,7 @@ public class NotificacionService {
 
         n.setMensaje(mensaje);
         n.setUrl(url);
+        n.setFechaCreacion(LocalDateTime.now());
         repository.save(n);
     }
 
