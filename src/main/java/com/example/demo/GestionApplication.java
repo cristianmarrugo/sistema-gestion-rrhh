8383d416e19814732ebc8559dec7d4edf3781906
Cristian marrugo <cristian.maba2005@outlook.com>
2026-05-15 16:28:23 -0500

    feat: quitar comentarios

--- src/main/java/com/example/demo/GestionApplication.java
+++ src/main/java/com/example/demo/GestionApplication.java
@@ -12,14 +12,4 @@ public class GestionApplication {
 		SpringApplication.run(GestionApplication.class, args);
 	}
 
-//	# Este es el valor real que usará Docker
-//
-//
-//	LOCAL_DB_URL=jdbc:mysql://localhost:3307/rrhh_db?allowPublicKeyRetrieval=true&useSSL=false
-//	DB_USERNAME=admin
-//			DB_PASSWORD=admin123
-//	DB_ROOT_PASSWORD=root123
-//
-//	PYTHON_EXE_PATH=/usr/bin/python3
-
 }
