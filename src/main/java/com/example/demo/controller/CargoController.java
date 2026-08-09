e48064558c9f4e227b6454bb70eb7987a330610e
Cristian marrugo <cristian.maba2005@outlook.com>
2026-08-09 17:55:40 -0500

    feat: peticiones http para movil (api)

--- src/main/java/com/example/demo/controller/CargoController.java
+++ src/main/java/com/example/demo/controller/CargoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/cargos")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class CargoController {
 
     private final CargoService cargoService;
