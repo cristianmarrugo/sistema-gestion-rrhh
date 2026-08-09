f68fc7e949867c0711845020631e7092a8a291bb
Cristian marrugo <cristian.maba2005@outlook.com>
2026-08-09 18:10:12 -0500

    feat: anotacion @CrossOrigin(origins = *) para peticiones http

--- src/main/java/com/example/demo/controller/VacacionController.java
+++ src/main/java/com/example/demo/controller/VacacionController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/vacaciones")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class VacacionController {
 
     private final VacacionService vacacionService;
