4e721bf25f5315e6cf6f821b822d680c2efc7332
Cristian marrugo <cristian.maba2005@outlook.com>
2026-08-09 18:08:49 -0500

    feat:  anotación @CrossOrigin(origins = *) para peticiones http

--- src/main/java/com/example/demo/controller/TestProcesoController.java
+++ src/main/java/com/example/demo/controller/TestProcesoController.java
@@ -2,12 +2,14 @@ package com.example.demo.controller;
 
 import com.example.demo.service.TareaSalidaAutomatica;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/test-proceso") // Prefijo de la clase
+@CrossOrigin(origins = "*")
 public class TestProcesoController {
 
     @Autowired
