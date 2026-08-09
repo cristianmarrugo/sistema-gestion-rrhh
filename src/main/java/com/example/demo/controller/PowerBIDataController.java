e99ce6f0936b1cc9585a1f51e309e4c6bd9d869d
Cristian marrugo <cristian.maba2005@outlook.com>
2026-08-09 18:06:32 -0500

    feat: peticiones http para app movil

--- src/main/java/com/example/demo/controller/PowerBIDataController.java
+++ src/main/java/com/example/demo/controller/PowerBIDataController.java
@@ -19,6 +19,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/powerbi")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class PowerBIDataController {
 
     private final AsistenciaService asistenciaService;
